b39ab354d0d7254bb6d716ddd8459993f6d82396
Ben Stanley <benmastanley@gmail.com>
2025-07-23 18:23:50 +0100

    add table component

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>Finshark Design Page</h1>
+      <h2>
+        Welcome to the Design Page, this is where we will house various design
+        aspects of the app.
+      </h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
